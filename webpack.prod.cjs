e04d9a26557e9748eaf3477c4403eab761562c15
bakeza <dbakeza2002@gmail.com>
2024-09-03 19:07:54 +0300

    fix bugs

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -1,9 +1,8 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
+import HtmlWebPackPlugin from "html-webpack-plugin";
+import path from "path";
+import WorkboxPlugin from "workbox-webpack-plugin";
 
-module.exports = {
+export default {
   entry: "./src/client/index.js",
   mode: "production",
   module: {
@@ -11,7 +10,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: "babel-loader",
       },
       {
         test: /\.scss$/,
@@ -22,7 +21,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
-      filename: "./index.html",
+      filename: "index.html",
     }),
     new WorkboxPlugin.GenerateSW(),
   ],
